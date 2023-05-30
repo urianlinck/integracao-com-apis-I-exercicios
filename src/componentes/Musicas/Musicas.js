7265773944a5776e20f5b453e9a0e4c04103a84e
urianlinck <urianlinck@gmail.com>
2023-05-30 18:18:06 -0300

    pronto

--- src/componentes/Musicas/Musicas.js
+++ src/componentes/Musicas/Musicas.js
@@ -1,27 +1,57 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Botao, ContainerInputs, ContainerMusicas, InputMusica, Musica } from './styled'
+import axios from 'axios'
 
-const musicasLocal = [{
-    artist: "Artista 1",
-    id: "1",
-    name: "Musica1",
-    url: "http://spoti4.future4.com.br/1.mp3"
-},
-{
-    artist: "Artista 2",
-    id: "2",
-    name: "Musica2",
-    url: "http://spoti4.future4.com.br/2.mp3"
-},
-{
-    artist: "Artista 3",
-    id: "3",
-    name: "Musica3",
-    url: "http://spoti4.future4.com.br/3.mp3"
-}]
 
 export default function Musicas(props) {
-    const [musicas, setMusicas] = useState(musicasLocal)
+    const [musicas, setMusicas] = useState([])
+
+    const pegarMusicas = () => {
+        axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${props.playlist.id}/tracks`, {
+            headers: {
+                Authorization: 'urian-linck-faruqi'
+            }
+        })
+        .then((resposta)=>{
+            setMusicas(resposta.data.result.tracks)
+        })
+        .catch((erro)=>{
+            console.log(erro)
+        })
+    }
+    useEffect(()=>{
+        pegarMusicas()
+    }, [])
+
+    const [musica, setMusica] = useState("")
+    const [artista, setArtista] = useState("")
+    const [url, setUrl] = useState("")
+
+    const body = {
+        "name": musica, 
+        "artist": artista,
+        "url": url
+    }
+    const adicionarMusica = ()=>{
+        axios.post(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${props.playlist.id}/tracks`, body, {
+            headers: {
+                Authorization: 'urian-linck-faruqi'
+            }
+        })
+         }
+
+    const removerMusica = (musica) => {
+        axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${props.playlist.id}/tracks/${musica.id}`, {
+            headers: {
+                Authorization: 'urian-linck-faruqi'
+            }
+        })
+         
+    }
+
+    useEffect(()=>{
+        pegarMusicas()
+    }, [musicas, musica])
 
     return (
         <ContainerMusicas>
@@ -31,14 +61,14 @@ export default function Musicas(props) {
                     <Musica key={musica.id}>
                         <h3>{musica.name} - {musica.artist}</h3>
                         <audio src={musica.url} controls />
-                        <button>X</button>
+                        <button onClick={()=>{removerMusica(musica)}}>X</button>
                     </Musica>)
             })}
             <ContainerInputs>
-                <InputMusica placeholder="artista" />
-                <InputMusica placeholder="musica" />
-                <InputMusica placeholder="url" />
-                <Botao>Adicionar musica</Botao>
+                <InputMusica onChange={(e)=>{setArtista(e.target.value)}} value={artista} placeholder="artista" />
+                <InputMusica onChange={(e)=>{setMusica(e.target.value)}} value={musica}placeholder="musica" />
+                <InputMusica onChange={(e)=>{setUrl(e.target.value)}} value={url} placeholder="url" />
+                <Botao onClick={adicionarMusica}>Adicionar musica</Botao>
             </ContainerInputs>
         </ContainerMusicas>
     )
--- src/componentes/Playlists/Playlists.js
+++ src/componentes/Playlists/Playlists.js
@@ -1,27 +1,27 @@
-import React, {  useState } from "react";
+import React, {  useEffect, useState } from "react";
 import Musicas from "../Musicas/Musicas";
+import axios from "axios";
 
-const playlistsLocal = [
-    {
-        id: 1,
-        name: "Playlist 1"
-    },
-    {
-        id: 2,
-        name: "Playlist 2"
-    },
-    {
-        id: 3,
-        name: "Playlist 3"
-    },
-    {
-        id: 4,
-        name: "Playlist 4"
-    },
-]
 function Playlists() {
-    const [playlists, setPlaylists] = useState(playlistsLocal)
-  
+    const [playlists, setPlaylists] = useState([])
+    
+    const pegarPlaylist = () => {
+        axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists', {headers:{
+            Authorization: 'urian-linck-faruqi'
+        }})
+        .then((resposta)=>{
+            setPlaylists(resposta.data.result.list)
+            // setTimeout(() => {
+            //     console.log(playlists)                
+            // }, 1000);
+        })
+        .catch((erro)=>{
+            console.log(erro)
+        })
+    }
+    useEffect(()=>{
+        pegarPlaylist()
+    }, [])
     return (
         <div>
             {playlists.map((playlist) => {
